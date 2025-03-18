34d35ca2bd67ac6cc7b18631134822b829aadca2
sevanbbx <sevanbbx@gmail.com>
2025-03-18 16:11:26 +0400

    reverse index

--- cpp/15_03_2025/classwork/7.access_to_tuple_from_right.cpp
+++ cpp/15_03_2025/classwork/7.access_to_tuple_from_right.cpp
@@ -11,6 +11,7 @@ struct tuple<HeadType, BodyTypes...> {
     typedef HeadType head_type;
     typedef tuple<BodyTypes...> inner_type;
 
+    static constexpr int size = 1 + tuple<BodyTypes...>::size;
     HeadType _head;
     inner_type _body;
 
@@ -21,6 +22,7 @@ struct tuple<HeadType, BodyTypes...> {
 
 template<>
 struct tuple<> {
+    static constexpr int size = 0;
 };
 
 template<typename TupleType, unsigned int Index>
@@ -52,6 +54,26 @@ struct getter<TupleType, 0> {
     }
 };
 
+// Helper struct to calculate the correct index from the right
+template<unsigned int Size, unsigned int Index>
+struct right_index {
+    static constexpr unsigned int value = Size - Index - 1;
+};
+
+template<typename TupleType, unsigned int Index>
+struct right_getter {
+    static constexpr unsigned int adjusted_index = right_index<TupleType::size, Index>::value;
+    typedef typename getter<TupleType, adjusted_index>::index_type index_type;
+
+    static const index_type& get(const TupleType& t) {
+        return getter<TupleType, adjusted_index>::get(t);
+    }
+
+    static index_type& get(TupleType& t) {
+        return getter<TupleType, adjusted_index>::get(t);
+    }
+};
+
 
 template<unsigned int Index, typename TupleType>
 const typename getter<TupleType, Index>::index_type& get(const TupleType& t) {
@@ -63,10 +85,23 @@ typename getter<TupleType, Index>::index_type& get(TupleType& t) {
     return getter<TupleType, Index>::get(t);
 }
 
+
+template<unsigned int Index, typename TupleType>
+const typename right_getter<TupleType, Index>::index_type& right_get(const TupleType& t) {
+    return right_getter<TupleType, Index>::get(t);
+}
+
+template<unsigned int Index, typename TupleType>
+typename right_getter<TupleType, Index>::index_type& right_get(TupleType& t) {
+    return right_getter<TupleType, Index>::get(t);
+}
+
 int main() {
     tuple<float, std::string, int> t(5.2f, "letter", 61);
     std::cout << "The second item is: "<< get<1>(t) << std::endl;
     get<2>(t) += 12;
     std::cout << "The third item is: " << get<2>(t) << std::endl;
+    std::cout << "t.size = " << t.size << std::endl;
+    std::cout << "The last item is: " << right_get<1>(t) << std::endl;
     return 0;
 }
